90dfe8eaa13540a5a3c5054dfe67ccb2e30b501d
김경록 <goodboy3421@gmail.com>
2019-12-08 13:02:02 +0900

    [ADD] 숫자야구 게임시스템 기능

--- src/main/java/com/precourse/BaseballSystem.java
+++ src/main/java/com/precourse/BaseballSystem.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
  * 숫자야구게임의 전체적인 시스템을 구축하는 클래스
  */
 public class BaseballSystem {
+    private static int tryNumber = 0;
+    private static boolean incorrect = true;
     private static ArrayList<Integer> answer = new ArrayList<>();
     private static ArrayList<Integer> insert = new ArrayList<>();
 
@@ -14,11 +16,14 @@ public class BaseballSystem {
      * 숫자야구게임을 시작한다.
      */
     public void playBaseball() {
-            answer = NumberGenerator.numberGenerator();
+        answer = NumberGenerator.numberGenerator();
 
-            while (true) {
-                insert = InputNumber.insertList;
-            }
+        while (incorrect) {
+            tryNumber++;
+            insert = InputNumber.insertNumber();
+            ComparedResult comparedResult = new ComparedResult();
+            incorrect = comparedResult.compareResult(answer, insert, tryNumber);
+        }
     }
 
 }
