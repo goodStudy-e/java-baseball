0df2e6c0e1a07ce5a828e39f651fee85716d8142
ZhenxiKIm <8055kjh@gmail.com>
2019-12-08 23:14:14 +0900

    10번 시도 수정

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,10 +1,7 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class test {
-    //수비 : 0~9중 4개의 수 중복되지 않게 뽑기
+    //수비 : 0~9중 4개의 수 뽑기
     //공격자 :  10번의 기회 안에 4개의 수 맞추기
     //숫자 맞으면 볼, 틀리면 스트라이크
     public static void main(String[] args) {
@@ -13,30 +10,37 @@ public class test {
         int strike = 0;
         int ball = 0;
         int count = 0; //횟수 체크
-        int [] defenceArr = new int[4];
+        int[] defenceArr = new int[4];
 
         //수비 숫자 설정
-        for (int i=0;i<4; i++) {
-        // defenceArr[i] = (int) ((Math.random() * 10) + 1);
+        for (int i = 0; i < 4; i++) {
+            // defenceArr[i] = (int) ((Math.random() * 10) + 1);
             //수비 중복없이
-            int tempNum = (int)((Math.random() * 10) + 1);
+            defenceArr[i] = (int) ((Math.random() * 10) + 1);
             //비교
-            for(int j=0;j<defenceArr.length;j++){
-                if(defenceArr[i] == tempNum) break;
-            }
-            //수비 숫자 입력
-            defenceArr[i] = tempNum;
         }
-        if(count == 10) {
+        while (count < 11) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("공격 숫자를 입력하세요");
+            System.out.println("공격 숫자 4자리를 입력하세요");
+
             count++;
             int attackNum = scanner.nextInt();
+            String str = String.valueOf(attackNum);
 
+            if (str.length() != 4) {
+                System.out.println("4자리를 다시 입력해주세요");
+                attackNum = scanner.nextInt();
+                str = String.valueOf(attackNum);
+
+            }
+
+
+            String[] numList = new String[4];
+            numList = str.split("");
             //수비 공격 비교
             for (int i = 0; i < defenceArr.length; i++) {
 
-                if (attackNum == defenceArr[i]) {
+                if (Integer.parseInt(numList[i]) == defenceArr[i]) {
                     System.out.println("스트라이크 입니다.");
                     strike++;
                 } else {
@@ -50,11 +54,13 @@ public class test {
                 }
             }
 
-
             System.out.println("결과는 strike" + strike + "회 ball은" + ball + "회 입니다");
 
 
         }
-        //TODO int, integer
+        if (count > 10) {
+            System.out.println("제한 횟수 10회를 초과하였습니다.");
+        }
+
     }
 }
